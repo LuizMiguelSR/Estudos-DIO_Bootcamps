d56784b32826fe731b3b1f491c7c6f0fc71dbf4c
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2022-12-10 19:25:10 -0300

    Update: Interpretaando pilha de exceção

---
+++ PHP-Experience/Exercicios/modulo-2/Debug_Tratamento_Excecoes/interpretaPilha.php
@@ -0,0 +1,24 @@
+<?php 
+    function f1() {
+        echo ">F1 está antes da exceção"."<br/>";
+    }
+
+    function f2($int) {
+        if(!is_int($int)) {
+            throw new Exception("O argumento não é o tipo esperado");
+        } else {
+            echo ">F2 está na exceção"."<br/>";
+        }
+
+        f3();
+    }
+
+    function f3() {
+        echo ">F3 está depois da exceção";
+    }
+
+    f1();
+    f2(
+        int: 5
+    );
+?>
---
+++ PHP-Experience/Exercicios/modulo-2/README.md
@@ -0,0 +1,11 @@
+# 📚 DIO Bootcamp PHP Experience 📚
+
+### [🔙 **VOLTAR**](../../../../../)
+
+## **Debug e Tratamento de Exceções em PHP**
+
+- ### **Interpretando uma pilha de exceções**
+
+  - [Exemplo prático de pilha de exceções](/PHP-Experience/Exercicios/modulo-2/Debug_Tratamento_Excecoes/interpretaPilha.php);
+
+&nbsp;
