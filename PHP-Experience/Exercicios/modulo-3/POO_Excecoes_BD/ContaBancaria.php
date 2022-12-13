48b18b0ada4c2046a452d5767d744ab749b08c35
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2022-12-13 16:09:11 -0300

    Update: Funcionalidades de métdos

--- PHP-Experience/Exercicios/modulo-3/POO_Excecoes_BD/ContaBancaria.php
+++ PHP-Experience/Exercicios/modulo-3/POO_Excecoes_BD/ContaBancaria.php
@@ -1,20 +1,106 @@
 <?php 
 
+    declare(strict_types=1);
+
     /** Criada a classe, Visibilidade e Propriedades da classe */
-    class ContaBancaria {                       
-        public $banco;
-        public $nomeTitular = 'Luiz Miguel';
-        public $numeroDaAgencia = '3467';
-        public $numeroConta;
-        public $saldo = 1000.00;
+    class ContaBancaria 
+    {                  
+        private string $banco;
+        private string $nomeTitular;
+        private string $numeroDaAgencia;
+        private string $numeroConta;
+        private float $saldo;
+
+        /** Criação de um método construtor */
+        public function __construct
+        (
+            string $banco, 
+            string $nomeTitular, 
+            string $numeroDaAgencia, 
+            string $numeroConta, 
+            float $saldo
+        ){
+            $this->banco = $banco;
+            $this->nomeTitular = $nomeTitular;
+            $this->numeroDaAgencia = $numeroDaAgencia;
+            $this->numeroConta = $numeroConta;
+            $this->saldo = $saldo;
+        }
+
+        /** Criação de métodos com visibilidade pública */
+        public function obterSaldo(): string
+        {
+            return 'Seu saldo atual é R$ '.$this->saldo;
+        }
+
+        public function depositar(float $valor): string 
+        {
+            $this->saldo += $valor;
+            return 'Depósito de R$ ' . $valor . ' realizado';
+        }
+
+        public function sacar(float $valor): string
+        {
+            $this->saldo -= $valor;
+            return 'Saque de R$ ' . $valor . ' realizado';
+        }
     }
 
     /** Criação do objeto e atribuição deste objeto a uma variável */
-    $conta = new ContaBancaria(); 
+    $conta1 = new ContaBancaria(
+        /**
+         * banco
+         * nommeTitular
+         * numeroAgencia
+         * numeroConta
+         * saldo
+         */
+        'Banco do Brasil',
+        'Luiz Miguel',
+        '8273',
+        '573854-10',
+        0
+    ); 
 
-    var_dump($conta->saldo);
+    var_dump($conta1);
 
-    $conta->saldo = 0;
+    $conta2 = new ContaBancaria(
+        /**
+         * banco
+         * nommeTitular
+         * numeroAgencia
+         * numeroConta
+         * saldo
+         */
+        'Caixa Economica',
+        'Fulano da Silva',
+        '84576',
+        '5178912-10',
+        300.00
+    ); 
 
-    var_dump($conta->saldo);
+    var_dump($conta2);
+    
+    /*
+    echo $conta->obterSaldo(); // retorna 0
+
+    echo PHP_EOL;
+
+    echo $conta->depositar(300.00);
+
+    echo PHP_EOL;
+
+    echo $conta->obterSaldo(); // retorna 300
+
+    echo PHP_EOL;
+
+    echo $conta->sacar(150.00);
+
+    echo PHP_EOL;
+
+    echo $conta->obterSaldo(); // retorna 150
+
+    echo PHP_EOL; 
+    */
+
 ?>
--- PHP-Experience/Exercicios/modulo-3/README.md
+++ PHP-Experience/Exercicios/modulo-3/README.md
@@ -7,3 +7,4 @@
 - ### **Trabalhando com orientação a objetos em PHP**
 
   - [O que são classes e propriedades](/PHP-Experience/Exercicios/modulo-3/POO_Excecoes_BD/ContaBancaria.php);
+  - [Quais as funcionalidades de Métodos](/PHP-Experience/Exercicios/modulo-3/POO_Excecoes_BD/ContaBancaria.php);
