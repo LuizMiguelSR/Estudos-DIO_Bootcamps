5b8c2a86a5fbb7bf41f1c69695086017f23b1578
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2022-12-15 16:36:41 -0300

    Update: Introdução a BD em PHP

---
+++ PHP-Experience/Exercicios/modulo-3/POO_Excecoes_BD/bancoDeDados/connect.php
@@ -0,0 +1,15 @@
+<?php 
+
+    declare(strict_types=1);
+
+    $pdo = null;
+
+    try {
+        $pdo = new PDO('mysql:host=mysql;dbname=exemplo', 'root', '12345');
+    } catch (Exception $e) {
+        echo $e ->getMessage();
+        die();
+    }
+
+    return $pdo;
+?>
