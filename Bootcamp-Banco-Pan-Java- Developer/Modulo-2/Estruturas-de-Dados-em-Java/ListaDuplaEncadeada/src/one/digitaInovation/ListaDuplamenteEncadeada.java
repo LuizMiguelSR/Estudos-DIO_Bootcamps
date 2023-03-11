d61d85bf8e81e018d162ab567d22d2ed4e4605cf
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-03-10 23:40:47 -0300

    Update: Implementação da classe ListaDuplamenteEncadeada(toString())

--- Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaDuplaEncadeada/src/one/digitaInovation/ListaDuplamenteEncadeada.java
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaDuplaEncadeada/src/one/digitaInovation/ListaDuplamenteEncadeada.java
@@ -79,4 +79,16 @@ public class ListaDuplamenteEncadeada<T> {
     public int size() {
         return tamanhoLista;
     }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; i < size(); i++) {
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]---->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
