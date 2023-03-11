2c4752f9ecddedc1f0f774ce4710c9f76d32aa20
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-03-10 23:48:47 -0300

    Update: Utilização da classe ListaDuplamenteEncadeada

--- Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaDuplaEncadeada/src/one/digitaInovation/Main.java
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaDuplaEncadeada/src/one/digitaInovation/Main.java
@@ -2,6 +2,23 @@ package one.digitaInovation;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ListaDuplamenteEncadeada<String> minhaListaEncadeada  = new ListaDuplamenteEncadeada<>();
+
+        minhaListaEncadeada.add("c1");
+        minhaListaEncadeada.add("c2");
+        minhaListaEncadeada.add("c3");
+        minhaListaEncadeada.add("c4");
+        minhaListaEncadeada.add("c5");
+        minhaListaEncadeada.add("c6");
+        minhaListaEncadeada.add("c7");
+
+        System.out.println(minhaListaEncadeada);
+
+        minhaListaEncadeada.remove(3);
+        minhaListaEncadeada.add(3, "99");
+
+        System.out.println(minhaListaEncadeada);
+        System.out.println(minhaListaEncadeada.get(3));
     }
 }
