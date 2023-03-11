09a954c779ee951163177348ace109f521b7fa10
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-03-10 23:10:58 -0300

    Update: Implementação da classe ListaDuplamenteEncadeada(add())

--- Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaDuplaEncadeada/src/one/digitaInovation/ListaDuplamenteEncadeada.java
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaDuplaEncadeada/src/one/digitaInovation/ListaDuplamenteEncadeada.java
@@ -17,6 +17,20 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    public void add(T elemento) {
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if (primeiroNo == null) {
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null) {
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index) {
         NoDuplo<T> noAuxiliar = primeiroNo;
         for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
