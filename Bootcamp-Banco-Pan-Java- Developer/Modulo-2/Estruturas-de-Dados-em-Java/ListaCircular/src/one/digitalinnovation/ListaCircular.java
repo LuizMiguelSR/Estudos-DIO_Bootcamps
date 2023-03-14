3b31de4629cbb8b6862fad06405b25712fe990a5
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-03-13 23:12:16 -0300

    Update: Implementação da classe ListaCircular(remove())

--- Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaCircular/src/one/digitalinnovation/ListaCircular.java
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-2/Estruturas-de-Dados-em-Java/ListaCircular/src/one/digitalinnovation/ListaCircular.java
@@ -6,6 +6,24 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private int tamanhoLista;
 
+    public void remove(int index) {
+        if (index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O índice é maior que o tamanho da lista.");
+        No<T> noAuxiliar = this.cauda;
+        if (index == 0) {
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        } else if (index == 1) {
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        } else {
+            for (int i = 0; i < index-1; i++) {
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
+
     public T get(int index) {
         return this.getNo(index).getConteudo();
     }
