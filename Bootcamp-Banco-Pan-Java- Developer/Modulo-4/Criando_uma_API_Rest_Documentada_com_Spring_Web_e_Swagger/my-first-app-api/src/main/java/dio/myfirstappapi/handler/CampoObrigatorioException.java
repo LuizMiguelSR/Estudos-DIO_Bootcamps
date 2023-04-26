4d15b3fa477cf368f238764eec5f5280c4cb2db6
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-04-26 19:46:36 -0300

    Update: Habilitando o tratamento de exceções de negócio com handlers

---
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-4/Criando_uma_API_Rest_Documentada_com_Spring_Web_e_Swagger/my-first-app-api/src/main/java/dio/myfirstappapi/handler/CampoObrigatorioException.java
@@ -0,0 +1,7 @@
+package dio.myfirstappapi.handler;
+
+public class CampoObrigatorioException extends BusinessException{
+    public CampoObrigatorioException(String campo) {
+        super("O campo %s é obrigatório", campo);
+    }
+}
--- Bootcamp-Banco-Pan-Java- Developer/Modulo-4/Criando_uma_API_Rest_Documentada_com_Spring_Web_e_Swagger/my-first-app-api/src/main/java/dio/myfirstappapi/repository/UsuarioRepository.java
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-4/Criando_uma_API_Rest_Documentada_com_Spring_Web_e_Swagger/my-first-app-api/src/main/java/dio/myfirstappapi/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package dio.myfirstappapi.repository;
 
+import dio.myfirstappapi.handler.BusinessException;
+import dio.myfirstappapi.handler.CampoObrigatorioException;
 import dio.myfirstappapi.model.Usuario;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,10 @@ import java.util.List;
 @Repository
 public class UsuarioRepository {
     public void save(Usuario usuario){
+        if(usuario.getLogin()==null)
+            throw new CampoObrigatorioException("login");
+        if(usuario.getPassword()==null)
+            throw new CampoObrigatorioException("password");
         if(usuario.getId()==null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         else
