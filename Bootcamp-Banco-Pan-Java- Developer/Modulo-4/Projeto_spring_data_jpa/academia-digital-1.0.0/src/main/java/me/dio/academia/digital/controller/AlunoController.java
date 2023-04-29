1bdc529e6ea52495908bd28703781d6913b1d918
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-04-28 21:50:30 -0300

    Update: Validadçoes

--- Bootcamp-Banco-Pan-Java- Developer/Modulo-4/Projeto_spring_data_jpa/academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ Bootcamp-Banco-Pan-Java- Developer/Modulo-4/Projeto_spring_data_jpa/academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -7,6 +7,7 @@ import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,7 @@ public class AlunoController {
     }
 
     @PostMapping
-    public Aluno create(@RequestBody AlunoForm form) {
+    public Aluno create(@Valid @RequestBody AlunoForm form) {
         return service.create(form);
     }
 
